3e85d339b40d877613921ff7e66e9d09099987b4
kosa <thddpfla1107@gmail.com>
2025-04-23 15:38:00 +0900

    test

--- basic_.c
+++ basic_.c
@@ -1,7 +1,23 @@
-#include <stdio.h>
+#include <stdio.h> // stdio.h 헤더파일 포함
 
-int main(void)
+int main(void) //main 함수의 시작
 {
-	printf("Hello, World! d\n");
-	return 0;
-}
+	/*
+		printf 함수는 모니터로 출려글 하는 경우에 쓴다.
+		인자로 문자열을 전달하면 문자열을 출력한다.
+	*/
+	//기본
+	//printf("Hello, World! d\n"); // 모니터로 문자열 출력
+
+	//문제1
+	//	printf("송예림\n");
+	//	printf("송 예림\n");
+	//	printf("송 예 림\n");
+
+	//문제2
+	printf("송예림\n");
+	printf("방배동\n");
+	printf("01031893944\n");
+
+	return 0; // 0을 반환한다.
+} //메인 함수의 끝
