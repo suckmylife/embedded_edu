6bfc2537f11c0f8ec1bde5b7c4562638dc74ab9d
kosa <thddpfla1107@gmail.com>
2025-04-24 14:21:50 +0900

    중간저장

--- c_0424/c_0424.c
+++ c_0424/c_0424.c
@@ -4,4 +4,95 @@
 
 int main()
 {
+	//61page
+	/*printf(" - 61 페이지 -\n");
+	int num1 = 12;
+	int num2 = 12;
+	printf("num1 : %d \n", num1);
+	printf("num1++ : %d \n", num1++);
+	printf("num1 : %d \n\n", num1);
+	printf("num2 : %d\n", num2);
+	printf("++num2 : %d \n", ++num2);
+	printf("num2 : %d \n\n", num2);*/
+
+	//66page
+	/*printf(" - 66 페이지 -\n");
+	int num1 = 10;
+	int num2 = 12;
+	int result1=0, result2=0, result3=0;
+	result1 = (num1 == 10 && num2 == 12);
+	result2 = (num1 < 12 || num2>12);
+	result3 = (!num1);
+	printf("result1 : %d \n", result1);
+	printf("result2 : %d \n", result2);
+	printf("result3 : %d \n\n", result3);*/
+
+	//67page
+	/*printf(" - 67 페이지 -\n");
+	int num1 = 1,  num2 = 2;
+	printf("Hello "),printf("world! \n");
+	num1++, num2++;
+	printf("%d ", num1), printf("%d", num2), printf("\n\n");*/
+
+	//71page
+	//printf(" - 71 페이지 -\n"); 
+	//int num1 = 0, num2 = 0, result =0;
+	//printf("정수 one:  ");
+	//scanf_s("%d", &num1); // 언제 바뀌었데....8년전만해도...
+	//printf("정수 two:  ");
+	//scanf_s("%d", &num2);
+
+	//result = num1 + num2;
+	//printf("%d + %d = %d \n", num1, num2, result);
+
+	//실수값 비교의 문제 
+	/*printf(" - 실수값 비교의 문제 -\n");
+	float f = 0.0F;
+	int i = 0;
+	for (f = 0.0; f < 100.0f; f += 0.1f, i++)
+	{
+		printf("f:%f\n", f);
+
+	}
+	printf("i : %d\n", i);*/
+
+	////93페이지
+	//printf(" - 93 페이지 -\n");
+	//int num1 = 15;
+	//int num2 = 20;
+	//int num3 = num1 & num2;
+	//printf("AND 연산의 결과 : %d \n", num3);
+
+	//98페이지
+	/*printf(" - 98 페이지 -\n");
+	int num = 15;
+	int result1 = num << 1;
+	int result2 = num << 2;
+	int result3 = num << 3;
+
+	printf("1칸 이동 결과 : %d \n", result1);
+	printf("2칸 이동 결과 : %d \n", result2);
+	printf("3칸 이동 결과 : %d \n", result3);*/
+
+	//109페이지 sizeof가 필요한 이유 
+	//printf(" - 109 페이지 -\n");
+	//char ch = 9;
+	//int inum = 1052;
+	//double dnum = 3.1415;
+
+	//printf("변수 ch의 크기 : %d \n", sizeof(ch));
+	//printf("변수 inum의 크기 : %d \n", sizeof(inum));
+	//printf("변수 dnum의 크기 : %d \n", sizeof(dnum));
+
+	//printf("char의 크기 : %d \n", sizeof(char));
+	//printf("int의 크기 : %d \n", sizeof(int));
+	//printf("long의 크기 : %d \n", sizeof(long));//비주얼스튜디오는 4바이트인데 비주얼코드는 8바이트 이렇게 바이트 차이가 난다. 
+	//printf("long long의 크기 : %d \n", sizeof(long long));
+	//printf("float의 크기 : %d \n", sizeof(float));
+	//printf("double의 크기 : %d \n", sizeof(double));
+	
+	//109페이지
+	printf(" - 109 페이지 -\n");
+
+	return 0;
 }
