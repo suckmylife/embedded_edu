bf80847ac4fdf32506e07a384afd18c34e0f92d5
kosa <thddpfla1107@gmail.com>
2025-04-28 17:11:52 +0900

    중간 저장

--- c_0428/c_pointer.c
+++ c_0428/c_pointer.c
@@ -16,6 +16,46 @@ void Swap(int n1, int n2)
 	printf("n1 n2 : %d %d \n", n1, n2);
 }
 
+void Swap_ref(int* ptr1, int* ptr2)
+{
+	int temp = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = temp;
+}
+
+int Call_by_value(int n)
+{
+	return n *= n;
+}
+
+void Call_by_reference(int* ptr)
+{
+	*ptr *= *ptr;
+}
+
+Swap3(int* ptr1, int* ptr2, int* ptr3)
+{
+	int temp = 0;
+	int arr[3] = { *ptr1, *ptr2, *ptr3 };
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = i + 1; j < 3; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	
+}
+
 int main()
 {
 
@@ -187,8 +227,169 @@ int main()
 
 	Swap(num1, num2);
 	printf("num1 num2 : %d %d \n", num1, num2);*/
+	
+	//317페이지
+	/*int num1 = 10;
+	int num2 = 20;
+	
+	printf("num1 num2 : %d %d \n", num1, num2);
+	Swap_ref(&num1, &num2);
+	printf("num1 num2 : %d %d \n", num1, num2);*/
+
+	//문제 1 
+	/*int num = 2;
+	printf("1. call by value : %d \n", Call_by_value(num));
+
+	int num2 = 4;
+	Call_by_reference(&num2);
+	printf("1. call by reterence : %d \n", num2);*/
+
+	//문제 2
+	/*int num1 = 2;
+	int num2 = 1;
+	int num3 = 3;
+	Swap3(&num1, &num2, &num3);*/
 
 	
+	//도전1 
+	//int num[10];
+	//int h[10]; // 애네를 안쓰는 방법이 있데..
+	//int j[10];
+	//int h_cnt = 0;
+	//int j_cnt = 0;
+	//printf("총 10개의 숫자 입력 \n");
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	printf("입력 : ");
+	//	scanf_s("%d", &num[i]);
+	//	printf("\n");
+	//}
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	if (num[i] % 2 == 0)
+	//	{
+	//		h[h_cnt] = num[i];
+	//		h_cnt++;
+	//	}
+	//	else
+	//	{
+	//		j[j_cnt] = num[i];
+	//		j_cnt++;
+	//	}
+	//}
+	//printf("짝수 출력 : ");
+	//for (int i = 0; i < h_cnt; i++)
+	//{
+	//	printf("%d, ", h[i]);
+	//}
+	//printf("홀수 출력 : ");
+	//for (int i = 0; i < j_cnt; i++)
+	//{
+	//	printf("%d, ", j[i]);
+	//}
+
+	//도전 3 // 포인터를 어떻게 이용하라는건지 모르겟음
+	/*int num[10];
+	for (int i = 0; i < 10; i++) {
+		printf("입력 : ");
+		scanf_s("%d", &num[i]);
+	}
+	int* p = num;
+	for (int i = 0; i < 10; i++) {
+		if ((*(p+i) & 01) == 1)
+			printf(" %d", *(p + i));
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if ((*(p + i) & 01) == 0)
+			printf(" %d", *(p + i));
+		//선생님 코드
+		int num[10], a;
+		int* start = num;
+		int* end = num + 9;
+		for (int i = 0; i < 10; i++) {
+			printf("%d : ", i + 1), scanf("%d", &a);
+			if ((a & 01) == 0)*(end--) = a; else *(start++) = a;
+		}
+		printf("Print the Element of Array : [");
+		for (int i = 0; i < 10; i++) {
+			printf("%d, ", num[i]);
+		}
+		printf("\b\b]\n");
+	}*/
+
+
+	//도전 4
+	/*char c[10];
+	char* start = c;
+	char* end = NULL;
+	printf("문자열 입력 : ");
+	scanf_s("%s", &c,10);
+	int trigger = 1;
+	int c_cnt = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (c[i] == '\0')
+			break;
+		c_cnt++;
+	}
+	end = c + c_cnt - 1;
+	for (int i = 0; i < c_cnt/2; i++)
+	{
+		trigger = (*(end--) == *(start++)) ? 1 : 0;
+		if (trigger == 0)
+			break;
+	}
+	if (trigger == 0)
+		printf("회문이 아닙니다.");
+	else
+		printf("회문 입니다.");*/
+//선생님 코드 
+//char str[BUFSIZ];
+//int len = 0;
+//printf("Input some string : ");
+//scanf("%s", str);
+//
+//while (1) {
+//	if (str[len] == '\0')
+//		break;
+//	len++;
+//}
+//printf("Length : %d\n", len);
+//
+//for (int i = 0; i < len / 2; i++) {
+//	if (str[i] != str[len - i - 1]) {
+//		printf("This is not Palindrome\n");
+//		break;
+//	}
+//}
+	//339 page 
+	/*int arr1[3][4];
+	int arr2[7][9];
+	printf("세로3, 가로4 : %d \n", sizeof(arr1));
+	printf("세로7, 가로9 : %d \n", sizeof(arr2));*/
+
+	//342 page
+	/*int villa[4][2];
+	int popu, i, j;
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			printf("%d층 %d호 인구수 : ", i + 1, j + 1);
+			scanf_s("%d", &villa[i][j]);
+		}
+	}
+
+	for (i = 0; i < 4; i++)
+	{
+		popu = 0;
+		popu += villa[i][0];
+		popu += villa[i][1];
+		printf("%d층 인구수 : %d \n", i + 1, popu);
+	}*/
 
 	return 0;
 }
