eecf4f75326aca964a9000e0bd854738d2195e79
kosa <thddpfla1107@gmail.com>
2025-05-01 10:13:26 +0900

    class start

---
+++ c_0501/c_struct_point.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+void ClearLineFromReadBuffer(void)
+{
+    while (getchar() != '\n');
+}
+
+struct point
+{
+    int xpos;
+    int ypos;
+};
+
+struct person
+{
+    char name[20];
+    char phoneNum[20];
+    int age;
+};
+int main()
+{
+    
+}
