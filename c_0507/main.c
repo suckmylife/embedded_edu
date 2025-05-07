f9d63bbd5c0334bc116377dd0c01bf60fe8d8997
kosa <thddpfla1107@gmail.com>
2025-05-07 09:20:31 +0900

    start class

---
+++ c_0507/main.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+//typedef struct div
+//{
+//	int quotient;
+//	int remainder;
+//}Div;
+
+extern Div IntDiv(int num1, int num2);
+
+int main()
+{
+	Div val = IntDiv(5, 2);
+	printf("몫 : %d \n", val.quotient);
+	printf("나머지 : %d \n", val.remainder);
+	return 0;
+}
